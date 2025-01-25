35317ccfe50e37e6eabc5426ac20fae8c1fd452f
mgomezpaz <gomezpaz.mat@gmail.com>
2025-01-25 15:16:06 -0700

    created the pawn moves, still working on it

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -188,18 +188,139 @@ public interface PieceMovesCalculator {
         }
     }
 
+    /* 
+    DONE: Implement rook moves, 
+    this is basically bishops  
+    we can use the same logic as bishop but with the king directions
+    */
+
     class RookMovesCalculator implements PieceMovesCalculator {
         @Override
         public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
-            return new ArrayList<>(); // TODO: Implement rook moves
+            List<ChessMove> moves = new ArrayList<>();
 
+            // get possible directions for the rook
+            int[][] rook_rules = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+            
+            for (int[] rook_direction : rook_rules) {
+                int row = position.getRow();
+                int col = position.getColumn();
+                
+                while (true) {
+                    row += rook_direction[0];
+                    col += rook_direction[1];
+
+                    // make sure that the move is in the board
+                    if (col < 1 || col > 8 || row < 1 || row > 8) {
+                        break;
+                    }
+                    
+                    // find information about the new position being considered
+                    ChessPosition newPosition = new ChessPosition(row, col);
+                    ChessPiece newPositionStatus = board.getPiece(newPosition);
+                    
+                    // if the position is empty, it is a possible move
+                    if (newPositionStatus == null) {
+                        moves.add(new ChessMove(position, newPosition, null));
+                        continue;
+                    }
+                    
+                    // if the position is not empty, make sure it is an enemy piece
+                    if (newPositionStatus.getTeamColor() != board.getPiece(position).getTeamColor()) {
+                        moves.add(new ChessMove(position, newPosition, null));
+                    }
+                    break;
+                }
+            }
+            
+            return moves;
         }
     }
+
+    /* 
+    DONE: Implement pawn moves, 
+    this is more complex than the other pieces, 
+    we need to check if the pawn is moving forward, 
+    if it is, we need to check if the position is empty or has a piece
+    it can also move diagonally to kill another piece
+    */
 
     class PawnMovesCalculator implements PieceMovesCalculator {
         @Override
         public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
-            return new ArrayList<>(); // TODO: Implement pawn moves
+            List<ChessMove> moves = new ArrayList<>();
+            // get the color of the pawn
+            ChessGame.TeamColor color = board.getPiece(position).getTeamColor();
+            int starting_position = position.getRow();
+
+            // initialize possible directions for the pawn
+            int[][] pawn_rules;
+
+            // load possible directions for white pawns
+            if (color == ChessGame.TeamColor.WHITE) {
+                pawn_rules = new int[][]{{1, 0}, {1, 1}, {1, -1}, {2, 0}};
+            } 
+            // load possible directions for black pawns
+            else {  // must be BLACK
+                pawn_rules = new int[][]{{-1, 0}, {-1, 1}, {-1, -1}, {-2, 0}};
+            }
+            
+            // loop thorugh the directions
+            for (int[] pawn_direction : pawn_rules) {
+                int row = position.getRow();
+                int col = position.getColumn();
+                row += pawn_direction[0];
+                col += pawn_direction[1];
+
+                // make sure that the move is in the board
+                if (col < 1 || col > 8 || row < 1 || row > 8) {
+                    break;
+                }
+
+                // get information about the new position
+                ChessPosition newPosition = new ChessPosition(row, col);
+                ChessPiece newPositionStatus = board.getPiece(newPosition);
+
+    
+                // if moving forward by one
+                if (pawn_direction[1] == 0 && (pawn_direction[0] == 1 || pawn_direction[0] == -1)) {
+                    // if the position is empty, it is a possible move
+                    if (newPositionStatus == null) {
+                        moves.add(new ChessMove(position, newPosition, null));
+                        continue;
+                    }
+                }
+
+                // if moving diagonally
+                if (pawn_direction[1] == 1 || pawn_direction[1] == -1) {
+                    // if the position is not empty, make sure it is an enemy piece
+                    if (newPositionStatus.getTeamColor() != board.getPiece(position).getTeamColor()) {
+                        moves.add(new ChessMove(position, newPosition, null));
+                    }
+                }
+
+                // if moving by two
+                if (pawn_direction[1] == 0 && (pawn_direction[0] == 2 || pawn_direction[0] == -2)) {
+                    // if the pawn in the starting position as white
+                    if (color == ChessGame.TeamColor.WHITE && starting_position == 2) {
+                        // if the position is empty, it is a possible move
+                        if (newPositionStatus == null) {
+                            moves.add(new ChessMove(position, newPosition, null));
+                            continue;
+                        }
+                        
+                    //if the pawn in the starting position as black
+                    } else if (color == ChessGame.TeamColor.BLACK && starting_position == 7) {
+                        // if the position is empty, it is a possible move
+                        if (newPositionStatus == null) {
+                                moves.add(new ChessMove(position, newPosition, null));
+                            continue;
+                        }
+                    }
+                } 
+            }
+            
+            return moves;
         }
     }
 } 
