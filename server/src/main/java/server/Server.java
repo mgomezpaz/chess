1d169ab4b80e8d26a48c5dc5c5a9ea91a10331a7
mgomezpaz <gomezpaz.mat@gmail.com>
2025-03-03 15:04:38 -0700

    Understood Server and organized the Server Class

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -3,22 +3,47 @@ package server;
 import spark.*;
 
 public class Server {
+    private final Handler handler;
+
+    public Server() {
+        // create a new handler for all the endpoints
+        this.handler = new Handler();
+    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
 
+        // for the web files
         Spark.staticFiles.location("web");
 
-        // Register your endpoints and handle exceptions here.
+        // TODO: maybe add more error handling later?
+        
+        // all the endpoints we need for the chess server
+        // register
+        Spark.post("/user", handler::register);
+        // login
+        Spark.post("/session", handler::login);
+        // logout
+        Spark.delete("/session", handler::logout);
+        // list games
+        Spark.get("/game", handler::listGames);
+        // create game
+        Spark.post("/game", handler::createGame);
+        // join game
+        Spark.put("/game", handler::joinGame);
+        // clear db - for testing
+        Spark.delete("/db", handler::clear);
 
-        //This line initializes the server and can be removed once you have a functioning endpoint 
+        // start up the server
         Spark.init();
 
+        // wait until server is ready
         Spark.awaitInitialization();
         return Spark.port();
     }
 
     public void stop() {
+        // shut everything down when we're done
         Spark.stop();
         Spark.awaitStop();
     }
