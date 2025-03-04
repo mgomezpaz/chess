edbfbf174e7b92964afb1c3fb69d8e0644215edf
mgomezpaz <gomezpaz.mat@gmail.com>
2025-03-03 18:52:35 -0700

    modified main to intialize the server

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,19 @@
-import chess.*;
+import server.Server;
 
+/**
+ * Main class that starts the server
+ */
 public class Main {
+    /**
+     * Main method
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        // Create a new server
+        Server server = new Server();
+        // Run the server on port 8080
+        server.run(8080);
+        
+        System.out.println("Server started on port 8080");
     }
 }
